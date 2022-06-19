421c6c177671479809a85f42ac95e02b9108fb38
Aleksey Pasechnik <Arhellist@mail.ru>
2022-06-19 21:29:41 +0400

    Готовое решение

--- task.js
+++ task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result;
+    if (arr1.length === arr2.length && arr1.every((elemArr1, elemArr2) => elemArr1 === arr2[elemArr2])) {
+        result = true;
+    } else {
+        result = false;
+    }
+    return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+    let resultArr = arr.filter(val => val > 0 && val % 3 === 0).map(val => val * 10);
+    return resultArr; // array
 }
